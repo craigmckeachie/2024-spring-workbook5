eff32424c4cf74947e55a0c5a2ba50e85d0d95a9
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-13 13:36:01 -0400

    arrow function example

--- demos/arrow-functions.js
+++ demos/arrow-functions.js
@@ -1,8 +1,12 @@
 "use strict";
 
-function multiply(a,b) {
-    let result = a * b;
-    return result;
+//before
+function multiply(a, b) {
+  return a * b;
 }
 
 console.log(multiply(5, 5));
+
+//after
+let multiply2 = (a, b) => a * b;
+console.log(multiply2(5, 5));

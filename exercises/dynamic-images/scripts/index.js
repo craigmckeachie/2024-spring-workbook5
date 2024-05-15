642a558dc655c701a89972fc1198fe3949975663
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-15 15:33:58 -0400

    dynamic images 1

--- exercises/dynamic-images/scripts/index.js
+++ exercises/dynamic-images/scripts/index.js
@@ -5,62 +5,59 @@
 let imageFiles = [
   {
     id: "1",
-    path: "https://placehold.co/300x300",
+    path: "https://placehold.co/300x300?text=Placeholder+1",
     description: "placeholder 1",
   },
   {
     id: "2",
-    path: "https://placehold.co/300x300",
+    path: "https://placehold.co/300x300?text=Placeholder+2",
     description: "placeholder 2",
   },
   {
     id: "3",
-    path: "https://placehold.co/300x300",
+    path: "https://placehold.co/300x300?text=Placeholder+3",
     description: "placeholder 3",
   },
   {
     id: "4",
-    path: "https://placehold.co/300x300",
+    path: "https://placehold.co/300x300?text=Placeholder+4",
     description: "placeholder 4",
   },
   {
     id: "5",
-    path: "https://placehold.co/300x300",
+    path: "https://placehold.co/300x300?text=Placeholder+5",
     description: "placeholder 5",
   },
   {
     id: "6",
-    path: "https://placehold.co/300x300",
+    path: "https://placehold.co/300x300?text=Placeholder+6",
     description: "placeholder 6",
   },
   {
     id: "7",
-    path: "https://placehold.co/300x300",
+    path: "https://placehold.co/300x300?text=Placeholder+7",
     description: "placeholder 7",
   },
   {
     id: "8",
-    path: "https://placehold.co/300x300",
+    path: "https://placehold.co/300x300?text=Placeholder+8",
     description: "placeholder 8",
   },
   {
     id: "9",
-    path: "https://placehold.co/300x300",
+    path: "https://placehold.co/300x300?text=Placeholder+9",
     description: "placeholder 9",
   },
   {
     id: "10",
-    path: "https://placehold.co/300x300",
+    path: "https://placehold.co/300x300?text=Placeholder+10",
     description: "placeholder 10",
   },
 ];
 
-
-window.onload = function(){
+window.onload = function init() {
   //get all html elements into variables
-
   //define  your functions
-
-
   //associate your functions with html element events
-}
+  // HTMLElement.event = function
+};
